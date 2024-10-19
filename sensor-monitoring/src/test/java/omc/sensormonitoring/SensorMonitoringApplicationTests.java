53917e0fc353183d881fda580908e70e775c278a
allancrowley <allancrowleydev@gmail.com>
2024-10-19 13:53:58 +0300

    Initial commit

---
+++ sensor-monitoring/src/test/java/omc/sensormonitoring/SensorMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package omc.sensormonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
