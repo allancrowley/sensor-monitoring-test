1eb55883257a9ac95518a03359bfd4ffdb907c16
allancrowley <allancrowleydev@gmail.com>
2024-10-19 18:28:24 +0300

    implements aggregation DB query and conditional removal

--- sensor-monitoring/src/main/java/omc/sensormonitoring/repository/FaceAvgRepository.java
+++ sensor-monitoring/src/main/java/omc/sensormonitoring/repository/FaceAvgRepository.java
@@ -2,7 +2,14 @@ package omc.sensormonitoring.repository;
 
 import omc.sensormonitoring.model.SensorFaceData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FaceAvgRepository extends JpaRepository<SensorFaceData, Long> {
+
+    @Query("SELECT s FROM SensorFaceData s WHERE s.timestamp >= :timestamp")
+    List<SensorFaceData> findAllFromPeriod(@Param("timestamp") long timestamp);
 
 }
--- sensor-monitoring/src/main/java/omc/sensormonitoring/repository/SensorRepository.java
+++ sensor-monitoring/src/main/java/omc/sensormonitoring/repository/SensorRepository.java
@@ -1,10 +1,29 @@
 package omc.sensormonitoring.repository;
 
+import jakarta.transaction.Transactional;
 import omc.sensormonitoring.model.SensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 
 
 public interface SensorRepository extends JpaRepository<SensorData, Long> {
+
+    @Query("SELECT new SensorData(s.id, MAX(s.timestamp), s.face, ROUND(AVG(s.temperature), 2)) " +
+            "FROM SensorData s " +
+            "WHERE s.timestamp BETWEEN :startTimestamp AND :endTimestamp " +
+            "GROUP BY s.id, s.face")
+    List<SensorData> findAggregatedSensorData(@Param("startTimestamp") long startTimestamp,
+                                              @Param("endTimestamp") long endTimestamp);
+
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM SensorData s WHERE s.timestamp BETWEEN :startTimestamp AND :endTimestamp")
+    void deleteSensorDataInRange(@Param("startTimestamp") long startTimestamp,
+                                 @Param("endTimestamp") long endTimestamp);
 
 }
