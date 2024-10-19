2c8c3395674447a948bc7850cf13e0f014055a8b
allancrowley <allancrowleydev@gmail.com>
2024-10-19 14:08:52 +0300

    sensor-imitator initial

---
+++ sensor-imitator/src/main/java/omc/sensorimitator/SensorImitatorApplication.java
@@ -0,0 +1,13 @@
+package omc.sensorimitator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorImitatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorImitatorApplication.class, args);
+	}
+
+}
---
+++ sensor-imitator/src/test/java/omc/sensorimitator/SensorImitatorApplicationTests.java
@@ -0,0 +1,13 @@
+package omc.sensorimitator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorImitatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
