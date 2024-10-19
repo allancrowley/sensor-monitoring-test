8cab7d093c193e10c27950e27fc803aa6c19182c
allancrowley <allancrowleydev@gmail.com>
2024-10-19 20:01:11 +0300

    adds error handling and validation logic

--- sensor-monitoring/src/main/java/omc/sensormonitoring/dto/SensorDataDto.java
+++ sensor-monitoring/src/main/java/omc/sensormonitoring/dto/SensorDataDto.java
@@ -2,6 +2,9 @@ package omc.sensormonitoring.dto;
 
 
 import omc.sensormonitoring.model.FaceDirection;
+import jakarta.validation.constraints.NotNull;
+import static omc.sensormonitoring.controller.handler.ErrorMessages.*;
+
 
 /**
  * A data transfer object (DTO) representing sensor data.
@@ -12,21 +15,25 @@ public record SensorDataDto(
         /**
          * The unique identifier of the sensor.
          */
-        long id,
+        @NotNull(message=MISSING_SENSOR_ID_MESSAGE)
+        Long id,
 
         /**
          * The timestamp of when the sensor data was generated.
          * Represented as the number of milliseconds since the epoch (January 1, 1970).
          */
-        long timestamp,
+        @NotNull(message=MISSING_SENSOR_TIMESTAMP_MESSAGE)
+        Long timestamp,
 
         /**
          * The direction in which the sensor is facing, represented by an enum {@link FaceDirection}.
          */
+        @NotNull(message=MISSING_SENSOR_FACE_DIRECTION_MESSAGE)
         FaceDirection face,
 
         /**
          * The temperature recorded by the sensor, which may vary based on certain conditions.
          */
-        double temperature
+        @NotNull(message=MISSING_SENSOR_TEMPERATURE_MESSAGE)
+        Double temperature
 ) {}
